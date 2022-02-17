4a6f1a091d42c3dbe32a54757b632171efec7ea7
Kleber Freire <ch4r4d4@gmail.com>
2022-02-17 16:28:29 -0300

    create UpdateUserAvatar

--- src/modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController.ts
@@ -1,7 +1,15 @@
 import { Response, Request } from "express"
+import { container } from "tsyringe"
+import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase"
 
 class UpdateUserAvatarController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const {id, avatar_file} = request.body
+    
+    const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase)
+
+    updateUserAvatarUseCase.execute({user_id: id, avatar_file})
+
     return response.send()
   }
 }
